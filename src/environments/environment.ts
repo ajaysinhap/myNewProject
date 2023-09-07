ea8b992ee784b944c46a43fcc4dedcf615e20290
Ajaysinh Pawar <102040848+ajaysinhap@users.noreply.github.com>
2023-09-07 15:27:52 +0530

    fb

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'myapp-d0917',
+    appId: '1:1068759997751:web:82e96ca3647bf587d50eb2',
+    storageBucket: 'myapp-d0917.appspot.com',
+    apiKey: 'AIzaSyAZe94XjRhi4X4akGZduE2UYLiQr6I0ONM',
+    authDomain: 'myapp-d0917.firebaseapp.com',
+    messagingSenderId: '1068759997751',
+  },
+};
