efee9dc42fd0529b1bba324aa1e5c8963bf64d39
Ajaysinh Pawar <102040848+ajaysinhap@users.noreply.github.com>
2023-09-07 17:05:02 +0530

    firebaaseAdd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';        
+import { getAuth, provideAuth } from '@angular/fire/auth';        
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';        
+import { getStorage, provideStorage } from '@angular/fire/storage';        
+import { getAnalytics, provideAnalytics } from '@angular/fire/analytics'; 
+
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -12,6 +19,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),     
+    provideFirestore(() => getFirestore()),  
+    provideAuth(() => getAuth()),  
+    provideStorage(() => getStorage()),  
+    provideAnalytics(() => getAnalytics()),
 
    
   ],
